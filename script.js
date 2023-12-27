254cac525d022810f802624f5ec1fd542372bebd
keeeeeyu <keeeeeyu@Ubunto22.04.3.myguest.virtualbox.org>
2023-12-27 04:15:50 -0800

    elements added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
