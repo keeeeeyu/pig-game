93eeea057bbaaf9b0d7b7a0ca965f2d2068e55d8
keeeeeyu <keeeeeyu@Ubunto22.04.3.myguest.virtualbox.org>
2023-12-27 04:22:18 -0800

    btnroll handler added

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+// Rolling a random dice roll
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
